4ff9350ca9f2d17962ae93d92ddbc6c66c6fef28
MARC1999 <44403065+MARC1999@users.noreply.github.com>
2018-10-29 14:13:36 +0100

    Update Python_Jamon.py

--- Python/Python_Jamon.py
+++ Python/Python_Jamon.py
@@ -2,4 +2,4 @@ Comprar = input("indique su compra:")
 abducir = input("indique su abduccion:")
 estudiar = input("indique cuanto estudia:")
 if (jamon = "si") or ((abduccion ="si") and (dia = "J")) or (estudiar = "mucho")):
-  print ("aprobar M03")
+   print ("aprobar M03")
