f671907ed6fa692352e77c9c98e9b0fd7da5ed27
mvelascop <44403065+mvelascop@users.noreply.github.com>
2018-10-30 10:24:36 +0100

    Update Python_edats_3.py

--- Python/Python_edats_3.py
+++ Python/Python_edats_3.py
@@ -1,3 +1,3 @@
-teclat = input("Indique su edad:")
-if (edad <18> and (edad<=23) or (edad = 17):
+teclat = input("Indique la edad del teclado:")
+if (edad >=18 and (edad<=23) or (edad = 17):
     print("le decimos que puede entrar en la sesión de jóvenes")
