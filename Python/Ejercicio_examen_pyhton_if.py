a88363ca7abb6dddaf676b4bba4b038f2d627dff
mvelascop <44403065+mvelascop@users.noreply.github.com>
2018-11-06 15:08:20 +0100

    Create Ejercicio_examen_pyhton_if.py

---
+++ Python/Ejercicio_examen_pyhton_if.py
@@ -0,0 +1,14 @@
+Loteria= input("Te ha tocado la loteria?")
+if(loteria="si"):
+  print(cobras 3.000 euros al mes)
+else:
+  Milloneti= input("Es un milloneti?")
+  Edad= int(input("Indique su edad"))
+  Problemas de corazón= input("Tiene problemas de corazón")
+  Casado= input("Esta casado(a)?")
+if(edad>=80) and (problemas de corazón=“si”) and (casado="no")
+  print(me caso con un milloneti y aseguro la herencia)
+else:
+  estudiar= input("Has estudiado mucho?")
+  M01= int(input("Indique su nota en M01")
+  
