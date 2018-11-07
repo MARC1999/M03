7bd4eb492fca5791b3e0e08d47de4722c66e47fb
mvelascop <44403065+mvelascop@users.noreply.github.com>
2018-11-07 16:30:02 +0100

    Create Ejercicio_Comparador_de_años.py

---
+++ Python/Ejercicio_Comparador_de_años.py
@@ -0,0 +1,12 @@
+Anyo= int(input("Indique el anyo en el que estamos")
+Anyo= int(input("Escriba un anyo cualquiera")
+Anyo= int(input("Indique cuanto falta para llegar a 2020")
+if(anyo_actual==anyo_cualquiera):
+  print("Son el mismo anyo")
+else:
+if(anyo_actual<=anyo_cualquiera):
+  print("Para llegar a 2020 faltan 5 anyos")
+else:
+if(anyo_actual=2015) and (anyo_cualquiera=1999)
+  if(2015-1999=15)
+    print("Desde el 1999 han pasado 15 anyos")
