b0a59e1d3bf7b7e96d12098fa70731d7b42befb9
mvelascop <44403065+mvelascop@users.noreply.github.com>
2018-11-19 14:06:43 +0100

    Create ejercicio_pares.py

---
+++ Python/ejercicio_pares.py
@@ -0,0 +1,13 @@
+Lee un número del teclado
+Si es par: Escribe “Qué bonito número par”
+Si es impar: Escribe “Qué número más vulgar”
+Pista: usar módulo %
+
+
+numero = int(input("Escribe un numero")
+numero = int(input("Escribe un numero")
+if(16%8==0):
+    print("Qué bonito número par")
+else:
+    if(23%3==2):
+        print("Qué número más vulgar")
