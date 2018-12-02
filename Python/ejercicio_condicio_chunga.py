6a48c8d49173bb914e7c221e8801b4384d1e68ef
mvelascop <44403065+mvelascop@users.noreply.github.com>
2018-12-02 16:03:07 +0100

    Update ejercicio_condicio_chunga.py

--- Python/ejercicio_condicio_chunga.py
+++ Python/ejercicio_condicio_chunga.py
@@ -1,1 +1,13 @@
-df
+  numero=int(input("Elige un número cualquiera:"))
+if(numero%2==0)
+    print("Este número es un número par")
+else:
+    print("Este número és un número impar")
+    if(numero>=-10)and(numero<=40):
+        print("Este número está entre -10 y 40")
+        else:
+          print("Este número no está entre -10 y 40")
+             if(numero<0)
+                 print("Este número es negativo")
+             else:
+                 print("Este número no es negativo")
